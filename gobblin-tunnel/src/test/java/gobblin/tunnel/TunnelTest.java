be4dc7dfbbbd714111ad5804aabf25c41d427613
Nikolai Avteniev <nikolai.avteniev@gmail.com>
2015-10-16 01:00:48 -0400

    Cleanup

--- gobblin-tunnel/src/test/java/gobblin/tunnel/TunnelTest.java
+++ gobblin-tunnel/src/test/java/gobblin/tunnel/TunnelTest.java
@@ -25,10 +25,7 @@ import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertNotNull;
